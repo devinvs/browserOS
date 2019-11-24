f017e05ebf1ae4fbbd2afac693ae46a2b5aaa6df
Devin Vander Stelt <devin@vstelt.com>
2019-11-24 16:47:03 -0700

    Updates

---
+++ precache-manifest.b9607fdbc47242767b682df610db3d6d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee9012fec91f4967bd3d02a17ff4c0e",
+    "url": "/browserOS/index.html"
+  },
+  {
+    "revision": "f22f4c8af487a2ce5c32",
+    "url": "/browserOS/static/css/main.d16dd363.chunk.css"
+  },
+  {
+    "revision": "d85066ecd540e8e4fa15",
+    "url": "/browserOS/static/js/2.f858735e.chunk.js"
+  },
+  {
+    "revision": "f22f4c8af487a2ce5c32",
+    "url": "/browserOS/static/js/main.4624e9fa.chunk.js"
+  },
+  {
+    "revision": "dcef562dd9991c1171e2",
+    "url": "/browserOS/static/js/runtime-main.a28680ed.js"
+  },
+  {
+    "revision": "13e8c355d4e57aefd9cb181ce727e96d",
+    "url": "/browserOS/static/media/helloWorld.13e8c355.png"
+  },
+  {
+    "revision": "6d04cc096b44bde6c8e2e1ca319f000e",
+    "url": "/browserOS/static/media/terminal.6d04cc09.png"
+  }
+]);
